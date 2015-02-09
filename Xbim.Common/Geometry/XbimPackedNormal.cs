eac54943d12b755669fecd073026adaf79089b7d
SteveLockley <steve.lockley@northumbria.ac.uk>
2015-02-09 13:01:17 +0000

    XbimPacked normal error fixed with incorrect normal unpacking, test case added

--- Xbim.Common/Geometry/XbimPackedNormal.cs
+++ Xbim.Common/Geometry/XbimPackedNormal.cs
@@ -35,51 +35,66 @@ namespace Xbim.Common.Geometry
         public XbimPackedNormal(double x, double y, double z)
         {
             const double tolerance = 1e-4;
-            
-            double longitude;
-            if (Math.Abs(x) < tolerance && Math.Abs(y) < tolerance)
-            {
-                longitude = 0;
-                // latitude = 1;
-            }
-            else
-            {
-                longitude = Math.Acos(x / Math.Sqrt(x * x + y * y)) * (y < 0 ? -1 : 1);
-            }
-            double latitude = Math.Acos(z) * (z < 0 ? -1 : 1);
-            //long and lat are between -1 and 1
-            longitude = Math.Abs((longitude + Math.PI) / (2 * Math.PI));
-            latitude = Math.Abs((latitude + Math.PI) / (2 * Math.PI));
 
-            var u = (int)(longitude * PackSize);
-            var v = (int)(latitude * PackSize);
+            if (Math.Abs(y) < tolerance || Math.Abs(y - 1) < tolerance)
+            {
+                byte half = (byte)PackSize/2;
+                _packedData = (ushort)(half << 8 | (byte)y); 
+               
+            }
+
+            var lon = Math.Abs(z) < tolerance ? Math.PI / 2 : (Math.Atan(x / z));
+
+            // Need to correct Atan's value based on quadrant
+            if ((x >= 0 && z >= 0) || (x < 0 && z >= 0))
+            {
+                lon = (Math.PI / 2) - lon;
+            }
+            else if (x < 0 && z < 0)
+            {
+                lon = Math.PI * 1.5 - Math.Abs(lon);
+            }
+            else if (x >= 0 && z < 0)
+            {
+                lon = Math.PI * 1.5 + Math.Abs(lon);
+            }
+
+            var lat = Math.Acos(y);
+
+
+            lon = lon / (2 * Math.PI);
+            lat =  1 - lat / Math.PI;
+
+
+            //double longitude;
+            //if (Math.Abs(x) < tolerance && Math.Abs(y) < tolerance)
+            //{
+            //    longitude = 0;
+            //    // latitude = 1;
+            //}
+            //else
+            //{
+            //    longitude = Math.Acos(x / Math.Sqrt(x * x + y * y)) * (y < 0 ? -1 : 1);
+            //}
+            //double latitude = Math.Acos(z) * (z < 0 ? -1 : 1);
+            ////long and lat are between -1 and 1
+            //if (longitude < 0) longitude += 2*Math.PI;
+            //if (latitude < 0) latitude += 2 * Math.PI;
+            //longitude /= 2*Math.PI;
+            //latitude /= 2 * Math.PI;
+            ////longitude = Math.Abs((longitude + Math.PI) / (2 * Math.PI));
+            ////latitude = Math.Abs((latitude + Math.PI) / (2 * Math.PI));
+
+            var u = (int)(lon * PackSize);
+            var v = (byte)(lat * PackSize);
             _packedData = (ushort)(u << 8 | v); 
         }
 
         public XbimPackedNormal(XbimVector3D vec)
-        {
-           
-            const double tolerance = 1e-4;
-            vec.Normalize(); //make sure it is normalised   
-            double longitude;
-            if (Math.Abs(vec.X) < tolerance && Math.Abs(vec.Y) < tolerance)
-            {
-                longitude = 0;
-               // latitude = 1;
-            }
-            else
-            {
-                longitude = Math.Acos(vec.X / Math.Sqrt(vec.X * vec.X + vec.Y * vec.Y)) * (vec.Y < 0 ? -1 : 1);        
-            }
-            double latitude = Math.Acos(vec.Z) * (vec.Z < 0 ? -1 : 1);
-            //long and lat are between -1 and 1
-            longitude = Math.Abs((longitude + Math.PI) / (2 * Math.PI));
-            latitude = Math.Abs((latitude + Math.PI) / (2 * Math.PI));
-
-            var u = (int)(longitude * PackSize);
-            var v = (int)(latitude * PackSize);
-            _packedData = (ushort)(u << 8 | v);      
+            : this(vec.X, vec.Y, vec.Z)
+        {    
         }
+
         public byte U
         {
             get
@@ -100,16 +115,31 @@ namespace Xbim.Common.Geometry
         {
             get
             {
-                double u = ((U / PackSize) * (2 * Math.PI)) - Math.PI;
+                //double u = ((U / PackSize) * (2 * Math.PI)) ;
 
-                double v = ((V / PackSize) * (2 * Math.PI)) - Math.PI;
+                //double v = ((V / PackSize) * (2 * Math.PI)) ;
 
-                double x = (Math.Sin(v) * Math.Cos(u));
-                double y = (Math.Sin(v) * Math.Sin(u));
-                double z = Math.Cos(v);
-                var v3D = new XbimVector3D(x, y, z);
-                v3D.Normalize();
-                return v3D;
+                //double x = (Math.Sin(v) * Math.Cos(u));
+                //double y = (Math.Sin(v) * Math.Sin(u));
+                //double z = Math.Cos(v);
+                //var v3D = new XbimVector3D(x, y, z);
+                //v3D.Normalize();
+                //return v3D;
+                var u = U / PackSize;
+                var v = V / PackSize;
+                var theta = (v-1) * Math.PI * -1;
+                var sinTheta = Math.Sin(theta);
+                var cosTheta = Math.Cos(theta);
+
+                var phi = u * 2 * Math.PI;
+                var sinPhi = Math.Sin(phi);
+                var cosPhi = Math.Cos(phi);
+
+                var nX = cosPhi * sinTheta;
+                var nY = cosTheta;
+                var nZ = sinPhi * sinTheta;
+
+                return new XbimVector3D(nX, nY, nZ);
             }
         }
 
---
+++ Xbim.Essentials.Tests/GeometryTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xbim.Common.Geometry;
+
+namespace Xbim.Essentials.Tests
+{
+    [TestClass]
+    public class GeometryTests
+    {
+        [TestMethod]
+        public void PackedNormalTests()
+        {
+           // var vArray = new[] {new XbimVector3D(0, 0, 1), new XbimVector3D(0, 1, 0), new XbimVector3D(1, 0, 0), new XbimVector3D(1, 0, 0)};
+            var vectors = (List<XbimVector3D>)UniformPointsOnSphere(40);
+
+            foreach (var v in vectors)
+            {
+                var packed = new XbimPackedNormal(v);
+                var v2 = packed.Normal;
+                var a = v.CrossProduct(v2);
+                var x = Math.Abs(a.Length);
+                var y = v.DotProduct(v2);
+                var angle = Math.Atan2(x, y);
+                Assert.IsTrue(angle < 0.1);
+            }
+
+
+        }
+        IEnumerable<XbimVector3D> UniformPointsOnSphere(float n)
+        {
+            var points = new List<XbimVector3D>();
+            var i = Math.PI * (3 - Math.Sqrt(5));
+            var o = 2 / n;
+            for (var k = 0; k < n; k++)
+            {
+                var y = k * o - 1 + (o / 2);
+                var r = Math.Sqrt(1 - y * y);
+                var phi = k * i;
+                var v = new XbimVector3D(Math.Cos(phi)*r, y, Math.Sin(phi)*r);
+                points.Add(v);
+            }
+            return points;
+        }
+    }
+}
