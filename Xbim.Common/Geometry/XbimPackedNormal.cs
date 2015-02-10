f8756528dc16268b36db6aa43b2e411b7707a1f5
SteveLockley <steve.lockley@northumbria.ac.uk>
2015-02-10 16:59:36 +0000

    Bumped Nuget version for release

--- Xbim.Common/Geometry/XbimPackedNormal.cs
+++ Xbim.Common/Geometry/XbimPackedNormal.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
 
 namespace Xbim.Common.Geometry
 {
