92fe39fb4b60f606f4a759612681a9d9403d2e6a
Claudio Benghi <claudio.benghi@gmail.com>
2015-05-24 18:32:47 +0100

    Added GeometrySupportLevel tests

---
+++ Xbim.Essentials.Tests/GeometryCacheTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xbim.IO;
+
+namespace Xbim.Essentials.Tests
+{
+    [TestClass]
+    [DeploymentItem(@"GeometryCacheTestFiles\")]
+    public class GeometryCacheTests
+    {
+        [TestMethod]
+        public void FileVersionIsCorrect()
+        {
+            var m = new XbimModel();
+            m.Open("Monolith_v10.xBIM");
+            Assert.AreEqual(1, m.GeometrySupportLevel, "GeometrySupportLevel for Monolith_v10 should be 1");
+            m.Close();
+
+            m.Open("Monolith_v20.xBIM");
+            Assert.AreEqual(2, m.GeometrySupportLevel, "GeometrySupportLevel for Monolith_v20 should be 2");
+            m.Close();
+        }
+    }
+}
