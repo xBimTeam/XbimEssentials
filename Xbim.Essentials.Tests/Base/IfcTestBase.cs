6ef9e7b89a41cd6a5dd46e125eb76185575c257b
Andy Ward <andy@thewardhouse.net>
2015-06-24 10:42:54 +0100

    Refactored test to use a static model for testing Introduced test base class so tests are much cleaner

---
+++ Xbim.Essentials.Tests/Base/IfcTestBase.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Xbim.Common.Logging;
+using Xbim.IO;
+using Xbim.XbimExtensions;
+
+namespace Xbim.Essentials.Tests
+{
+    /// <summary>
+    /// A base class for unit testing IFC files. Provides a standard means of parsing and opening models for
+    /// Unit tests. 
+    /// </summary>
+    /// <remarks>Parsing is performed in ths constructor since ClassInitialize does not work on base classes under mstest</remarks>
+    public abstract class IfcTestBase
+    {
+        protected readonly ILogger Logger = LoggerFactory.GetLogger();
+
+        protected string XbimFile;
+
+        /// <summary>
+        /// The Model instance under test. Automatically opened fresh before each test method is run.
+        /// </summary>
+        protected XbimModel Model
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Constructs a Test Environment and initialises a model. The model is inferred from the Class name of the test fixture.
+        /// </summary>
+        public IfcTestBase()
+        {
+            var inferredModel = Path.ChangeExtension(this.GetType().Name, ".ifc");
+            InitialiseModel(inferredModel);
+        }
+
+        /// <summary>
+        /// Constructs a Test Environment and initialises a model with the given file. Use this overload if your
+        /// model is not named after your test fixture.
+        /// </summary>
+        public IfcTestBase(string ifcFile)
+        {
+            InitialiseModel(ifcFile);
+        }
+
+        /// <summary>
+        /// Parses the IFC file to xbim
+        /// </summary>
+        /// <remarks>Typically once per fixture</remarks>
+        /// <param name="ifcFile"></param>
+        public void InitialiseModel(String ifcFile)
+        {
+            if(!File.Exists(ifcFile))
+            {
+                var message = String.Format("Model '{0}' could not be located in '{1}'. \nCheck that you have applied a [DeploymentItem] attribute to the class and that the file is set to be copied on build.", 
+                    ifcFile,
+                    Environment.CurrentDirectory);
+                throw new FileNotFoundException(message,
+                    ifcFile);
+            }
+            Logger.DebugFormat(@"Opening model '{0}\{1}'", Environment.CurrentDirectory, ifcFile);
+            XbimFile = Path.ChangeExtension(ifcFile, ".xbim");
+            var model = new XbimModel();
+            // Parse the file and create .xbim
+            model.CreateFrom(ifcFile, XbimFile);
+        }
+
+        public static void CleanUpModel()
+        {
+            // TODO - delete xbim
+        }
+
+        /// <summary>
+        /// Runs before each test to provide a clean context
+        /// </summary>
+        [TestInitialize]
+        public void BeforeTest()
+        {
+            OpenModel(XbimFile);
+        }
+
+        /// <summary>
+        /// Runs after each test to explicitly close the model
+        /// </summary>
+        [TestCleanup]
+        public void AfterTest()
+        {
+            CloseModel();
+            // TODO: consider restoring xbim from a fresh version in case changes are made
+        }
+
+        /// <summary>
+        /// Loads Model with a fresh instance, in the desired access mode
+        /// </summary>
+        protected void OpenModel(string xBimFile, XbimDBAccess accessMode = XbimDBAccess.Read)
+        {
+            var model = new XbimModel();
+            model.Open(XbimFile, XbimDBAccess.ReadWrite);
+
+            if(Model != null)
+            {
+                CloseModel();
+            }
+            Model = model;
+
+        }
+
+        /// <summary>
+        /// Closes and disposes of model resources
+        /// </summary>
+        protected void CloseModel()
+        {
+            if (Model != null)
+            {
+                Model.Close();
+                Model.Dispose();
+            }
+        }
+
+    }
+}
--- Xbim.Essentials.Tests/IfcSIUnitTests.cs
+++ Xbim.Essentials.Tests/IfcSIUnitTests.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Xbim.Ifc2x3.MeasureResource;
-using Xbim.IO;
-using Xbim.Ifc2x3.Kernel;
-using Xbim.Ifc2x3.Extensions;
-using System.IO;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
 
 namespace Xbim.Essentials.Tests
 {
+    [DeploymentItem(@"TestSourceFiles\")]
     [TestClass]
-    public class IfcSIUnitTests
+    public class IfcSIUnitTests : IfcTestBase
     {
+        // Initialisation magic is done in IfcTestBase constructor and BeforeTest()
 
         /// <summary>
         /// Bug GH Issue#4 (Codeplex 
@@ -19,80 +15,12 @@ namespace Xbim.Essentials.Tests
         [TestMethod]
         public void ToString_Should_Equal_PrefixPlusName()
         {
-            var modelFile = CreateModelWithUnit();
-            var model = new XbimModel();
-            // reopen the IFC we just created
-            model.CreateFrom(modelFile, null, null, true);
+ 
+            var unit = Model.IfcProject.UnitsInContext.Units.First();
 
-            var unit = model.IfcProject.UnitsInContext.Units.First();
-
-            // Bug was the this returned AMPERE unless Activate had been called.
+            // Bug #4 was that this returned AMPERE unless Activate had been called.
             Assert.AreEqual("MILLIMETRE", unit.ToString());
-
-            model.Close();
-            model.Dispose();
-
-            DeleteFile(modelFile);
         }
-
-
-
-        private static string CreateModelWithUnit()
-        {
-            using (var model = XbimModel.CreateTemporaryModel())
-            {
-                CreateandInitModel(model);
-
-                var tempModel = Path.ChangeExtension(Path.GetTempFileName(), ".ifc");
-
-                model.SaveAs(tempModel);
-
-                return tempModel;
-            }
-        }
-
-        static private void CreateandInitModel(XbimModel model)
-        {
-            //Begin a transaction as all changes to a model are transacted
-            using (XbimReadWriteTransaction txn = model.BeginTransaction("Initialise Model"))
-            {
-                //do once only initialisation of model application and editor values
-                model.DefaultOwningUser.ThePerson.GivenName = "John";
-                model.DefaultOwningUser.ThePerson.FamilyName = "Bloggs";
-                model.DefaultOwningUser.TheOrganization.Name = "Department of Building";
-                model.DefaultOwningApplication.ApplicationIdentifier = "Construction Software inc.";
-                model.DefaultOwningApplication.ApplicationDeveloper.Name = "Construction Programmers Ltd.";
-                model.DefaultOwningApplication.ApplicationFullName = "Ifc Unit Tests";
-                model.DefaultOwningApplication.Version = "2.0.1";
-
-                //set up a project and initialise the defaults
-
-                var project = model.Instances.New<IfcProject>();
-
-                // Add the standard Units
-                project.Initialize(ProjectUnits.SIUnitsUK);
-                project.Name = "testProject";
-                project.OwnerHistory.OwningUser = model.DefaultOwningUser;
-                project.OwnerHistory.OwningApplication = model.DefaultOwningApplication;
-
-                // Sanity check first unit is what we expect
-                var firstUnit = ((IfcSIUnit)model.IfcProject.UnitsInContext.Units.First);
-                Assert.AreEqual(firstUnit.Name, IfcSIUnitName.METRE);
-                Assert.AreEqual(firstUnit.Prefix, IfcSIPrefix.MILLI);
-
-                txn.Commit();
-            }
-        }
-
-        private void DeleteFile(string modelFile)
-        {
-            try
-            {
-                File.Delete(modelFile);
-                File.Delete(Path.ChangeExtension(modelFile, ".xbim"));
-            }
-            catch
-            { }
-        }
+        
     }
 }
