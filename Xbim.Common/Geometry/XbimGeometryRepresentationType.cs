976721a8aa041ba8b0e7ccb1c614231cdb1d41c1
Claudio Benghi <claudio.benghi@gmail.com>
2015-07-27 13:16:15 +0100

    XbimGeometryRepresentationType now includes a clear distinction for OpeningsAndAdditions

--- Xbim.Common/Geometry/XbimGeometryRepresentationType.cs
+++ Xbim.Common/Geometry/XbimGeometryRepresentationType.cs
@@ -1,13 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Xbim.Common.Geometry
+﻿namespace Xbim.Common.Geometry
 {
+    /// <summary>
+    /// Discriminates representations on the application of boolean operations
+    /// </summary>
     public enum XbimGeometryRepresentationType
     {
+        /// <summary>
+        /// boolean operations with voids and extensions are included in the resulting representation
+        /// </summary>
         OpeningsAndAdditionsIncluded = 1,
-        OpeningsAndAdditionsExcluded = 2
+        /// <summary>
+        /// boolean operations with voids and extensions are excluded in the resulting representation
+        /// </summary>
+        OpeningsAndAdditionsExcluded = 2,
+        /// <summary>
+        /// representation of voids and extensions only 
+        /// </summary>
+        OpeningsAndAdditionsOnly = 4
     }
 }
