cc57dc8ff7be7adc70049f2fffc565fd8f188dd0
Martin Cerny <martin1cerny@gmail.com>
2015-10-27 14:58:46 +0100

    Parallel IFC4 generated schema added. This contains definition of interface for every IFC4 entity.

---
+++ Xbim.IO.Tests/InterTypeTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Xbim.MemoryModel.Tests
+{
+    [TestClass]
+    public class InterTypeTests
+    {
+        [TestMethod]
+        public void TestInterschemaTypeAssignment()
+        {
+            Ifc2x3.MeasureResource.IfcIdentifier a = "aaa";
+            Ifc4.MeasureResource.IfcIdentifier b = (string)a;
+
+            Assert.IsTrue(b == "aaa");
+        }
+    }
+}
---
+++ Xbim.IO.Tests/ReadIFC4InterfacesTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xbim.Ifc4.Interfaces;
+using Xbim.IO.Esent;
+using Xbim.IO.Memory;
+
+namespace Xbim.MemoryModel.Tests
+{
+    [TestClass]
+    [DeploymentItem("TestFiles")]
+    public class ReadIfc4InterfacesTest
+    {
+        [TestMethod]
+        public void LoadIfc4IntoMemoryTest()
+        {
+            var model = new MemoryModel<EntityFactory>();
+            model.Open("SampleHouse4.ifc");
+
+            var project = model.Instances.FirstOrDefault<IIfcProject>();
+            Assert.IsNotNull(project);
+            Assert.IsNotNull(project.Name);
+
+            var walls = model.Instances.OfType<IIfcWall>();
+            var doors = model.Instances.OfType<IIfcDoor>();
+            Assert.IsTrue(walls.Any());
+            Assert.IsTrue(doors.Any());
+        }
+
+        [TestMethod]
+        public void LoadIfc4IntoDbTest()
+        {
+            var model = new EsentModel(new EntityFactory());
+            model.CreateFrom("SampleHouse4.ifc", null, null, true, true);
+
+            var project = model.Instances.FirstOrDefault<IIfcProject>();
+            Assert.IsNotNull(project);
+            Assert.IsNotNull(project.Name);
+
+            var walls = model.Instances.OfType<IIfcWall>();
+            var doors = model.Instances.OfType<IIfcDoor>();
+            Assert.IsTrue(walls.Any());
+            Assert.IsTrue(doors.Any());
+        }
+    }
+}
