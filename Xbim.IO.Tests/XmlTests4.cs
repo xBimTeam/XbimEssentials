2603474d70e599f16b1af7f587a523264ae7870c
Martin Cerny <martin1cerny@gmail.com>
2015-11-20 18:39:27 +0100

    JSON serialization as an implementation of XmlWriter.

--- Xbim.IO.Tests/XmlTests4.cs
+++ Xbim.IO.Tests/XmlTests4.cs
@@ -15,6 +15,7 @@ using Xbim.Ifc4.PropertyResource;
 using Xbim.Ifc4.SharedBldgElements;
 using Xbim.Ifc4.StructuralLoadResource;
 using Xbim.Ifc4.UtilityResource;
+using Xbim.IO.JSON;
 using Xbim.IO.Memory;
 using Xbim.IO.Xml;
 using Xbim.IO.Xml.BsConf;
@@ -67,7 +68,10 @@ namespace Xbim.MemoryModel.Tests
 
                 var errs = ValidateIfc4(outPath);
                 Assert.AreEqual(0, errs);
+                
+                WriteJSON(model, "..\\..\\SampleHouse4.json");
             }
+
         }
 
         //[TestMethod]
@@ -126,7 +130,7 @@ namespace Xbim.MemoryModel.Tests
                 var errs = ValidateIfc4(outPath);
                 Assert.AreEqual(0, errs);
 
-                //var xmlString = File.ReadAllText(outPath);
+                WriteJSON(model, "..\\..\\properties.json");
             }
         }
         [TestMethod]
@@ -230,6 +234,31 @@ namespace Xbim.MemoryModel.Tests
                         //make sure all other objects will get written
                         .Concat(model.Instances);
                 
+                writer.Write(model, xml, all);
+                xml.Close();
+            }
+        }
+
+        private static void WriteJSON(IModel model, string path)
+        {
+            using (var xml = new JSONWritter(File.CreateText(path)))
+            {
+                var writer = new XbimXmlWriter4(configuration.IFC4Add1);
+                var project = model.Instances.OfType<IfcProject>();
+                var products = model.Instances.OfType<IfcObject>();
+                var relations = model.Instances.OfType<IfcRelationship>();
+
+                var all =
+                    new IPersistEntity[] { }
+                    //start from root
+                        .Concat(project)
+                    //add all products not referenced in the project tree
+                        .Concat(products)
+                    //add all relations which are not inversed
+                        .Concat(relations)
+                    //make sure all other objects will get written
+                        .Concat(model.Instances);
+
                 writer.Write(model, xml, all);
                 xml.Close();
             }
