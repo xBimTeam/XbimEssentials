62183132a6db11b5b1a5aa904d665defa230b2ee
Martin Cerny <martin1cerny@gmail.com>
2015-12-21 13:10:51 +0100

    View models ported from Essentials3 (Xbim.IO.ViewModels) to Essentials4 (Xbim.Ifc). It might be subject to move into Xbim.Presentation in the future.

---
+++ Xbim.Ifc/ViewModels/IXbimViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Xbim.Common;
+
+//using Xbim.IO;
+
+namespace Xbim.Ifc.ViewModels
+{
+    // todo: bonghi: need to add Parent property to ensure efficiency in node finding for trees.
+    public interface IXbimViewModel : INotifyPropertyChanged
+    {
+        IEnumerable<IXbimViewModel> Children { get; }
+        string Name {get;}
+        int EntityLabel { get; }
+        IPersistEntity Entity { get; }
+        IModel Model { get; }
+        bool IsExpanded { get; set; }
+        bool IsSelected { get; set; }
+        IXbimViewModel CreatingParent { get; set; } 
+    }
+}
