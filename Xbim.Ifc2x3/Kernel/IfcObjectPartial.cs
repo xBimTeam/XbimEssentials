a7c6954ccb244dcbdfdaa71b38610887018dcc8e
Steve Lockley <steve.lockley@northumbria.ac.uk>
2016-01-05 12:14:45 +0000

    Defining Type move to IfcObject

--- Xbim.Ifc2x3/Kernel/IfcObjectPartial.cs
+++ Xbim.Ifc2x3/Kernel/IfcObjectPartial.cs
@@ -52,6 +52,27 @@ namespace Xbim.Ifc2x3.Kernel
             }
         }
 
+        /// <summary>
+        /// Adds an element type to the object if it doesn't already have one, return the new or existing relationship that holds the type and this element. If there is a relationship for this type but this element is not related it adds it to the exosting relationship
+        /// </summary>
+        /// <param name="theType"></param>
+        /// <returns></returns>
+        public IfcRelDefinesByType AddDefiningType(IfcTypeObject theType)
+        {
+            var typedefs = Model.Instances.Where<IfcRelDefinesByType>(r => r.RelatingType == theType).ToList();
+            var thisTypeDef = typedefs.FirstOrDefault(r => r.RelatedObjects.Contains((this)));
+            if (thisTypeDef != null) return thisTypeDef; // it is already type related
+            var anyTypeDef = typedefs.FirstOrDefault(); //take any one of the rels of the type
+            if (anyTypeDef != null)
+            {
+                anyTypeDef.RelatedObjects.Add(this);
+                return anyTypeDef;
+            }
+            var newdef = Model.Instances.New<IfcRelDefinesByType>(); //create one
+            newdef.RelatedObjects.Add(this);
+            newdef.RelatingType = theType;
+            return newdef;
+        }
 
         /// <summary>
         /// Adds an existing property set to the objecty, NB no check is done for duplicate psets
--- Xbim.Ifc2x3/ProductExtension/IfcElementPartial.cs
+++ Xbim.Ifc2x3/ProductExtension/IfcElementPartial.cs
@@ -15,26 +15,6 @@ namespace Xbim.Ifc2x3.ProductExtension
             }
         }
 
-        /// <summary>
-        /// Adds an element type to the object if it doesn't already have one, return the new or existing relationship that holds the type and this element. If there is a relationship for this type but this element is not related it adds it to the exosting relationship
-        /// </summary>
-        /// <param name="theType"></param>
-        /// <returns></returns>
-        public IfcRelDefinesByType AddDefiningType(IfcElementType theType)
-        {
-            var typedefs = Model.Instances.Where<IfcRelDefinesByType>(r => r.RelatingType == theType).ToList();
-            var thisTypeDef = typedefs.FirstOrDefault(r => r.RelatedObjects.Contains((this)));
-            if (thisTypeDef != null) return thisTypeDef; // it is already type related
-            var anyTypeDef = typedefs.FirstOrDefault(); //take any one of the rels of the type
-            if (anyTypeDef != null)
-            {
-                anyTypeDef.RelatedObjects.Add(this);
-                return anyTypeDef;
-            }
-            var newdef = Model.Instances.New<IfcRelDefinesByType>(); //create one
-            newdef.RelatedObjects.Add(this);
-            newdef.RelatingType = theType;
-            return newdef;
-        }
+        
     }
 }
