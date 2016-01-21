de662ef1d3ca3ace44530be8625f29364dcb4af6
Steve Lockley <steve.lockley@northumbria.ac.uk>
2016-01-21 16:34:32 +0000

    IXbimCurve interfaces added

--- Xbim.Common/Geometry/IXbimCurve.cs
+++ Xbim.Common/Geometry/IXbimCurve.cs
@@ -1,10 +1,18 @@
-﻿namespace Xbim.Common.Geometry
+﻿using System.Collections.Generic;
+
+namespace Xbim.Common.Geometry
 {
     /// <summary>
     /// A wire that is open
     /// </summary>
     public interface IXbimCurve : IXbimGeometryObject
     {
-       
+        IEnumerable<XbimPoint3D> Intersections(IXbimCurve intersector);     
+        double Length { get; }
+        XbimPoint3D Start { get; }
+        XbimPoint3D End { get; }
+        double GetParameter(XbimPoint3D point);
+        XbimPoint3D GetPoint(double parameter);
+        bool IsClosed { get; }
     }
 }
