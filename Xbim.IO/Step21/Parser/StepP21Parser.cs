eff6a50bc1d0b0fe2430d9c4f70d0c4a09dd8f2e
Martin Cerny <martin1cerny@gmail.com>
2016-03-21 11:53:54 +0100

    Special token in lexer

--- Xbim.IO/Step21/Parser/StepP21Parser.cs
+++ Xbim.IO/Step21/Parser/StepP21Parser.cs
@@ -4,7 +4,7 @@
 
 // GPPG version 1.5.2
 // Machine:  C13300493
-// DateTime: 21.3.16 11:24:39
+// DateTime: 21.3.16 11:53:06
 // UserName: mxfm2
 // Input file <StepP21Parser.y - 18.3.16 10:10:08>
 
