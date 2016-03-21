6c168ca8346fbf97657b506b9ba39fe95d12a0b0
Martin Cerny <martin1cerny@gmail.com>
2016-03-21 14:47:15 +0100

    Almost identical

--- Xbim.IO/Step21/Parser/StepP21Parser.cs
+++ Xbim.IO/Step21/Parser/StepP21Parser.cs
@@ -4,7 +4,7 @@
 
 // GPPG version 1.5.2
 // Machine:  C13300493
-// DateTime: 21.3.16 14:08:18
+// DateTime: 21.3.16 14:43:02
 // UserName: mxfm2
 // Input file <StepP21Parser.y - 18.3.16 10:10:08>
 
