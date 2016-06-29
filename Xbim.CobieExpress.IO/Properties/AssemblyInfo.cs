e45469fe671029ae34d3a250e5015d716482af2c
Claudio Benghi <claudio.benghi@gmail.com>
2016-06-30 00:45:04 +0200

    Library versioning amended for Xbim.IO.TableStore and Xbim.CobieExpress.IO.

--- Xbim.CobieExpress.IO/Properties/AssemblyInfo.cs
+++ Xbim.CobieExpress.IO/Properties/AssemblyInfo.cs
@@ -6,13 +6,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Xbim.CobieExpress.IO")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Northumbria University")]
-[assembly: AssemblyProduct("Xbim.CobieExpress.IO")]
-[assembly: AssemblyCopyright("Copyright © Northumbria University 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +14,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ccf078e6-9045-4a7c-a576-27c024e15c9a")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
