d831b4cd59b76cc712fa4ef3ee8b68fe0649f79b
Martin Cerny <martin.cerny@xbim.cz>
2017-07-18 13:09:44 +0200

    Questionnaire prompt removed from the NuGet install script.

--- Install.ps1
+++ Install.ps1
@@ -11,9 +11,9 @@ $values.Add("IdeVersion", $project.DTE.Version)
 $webClient = New-Object System.Net.WebClient
 $webClient.UploadValuesAsync("http://xbimtracking.azurewebsites.net/api/log", $values)	
 
-$version = $package.Version
-$installed = [Environment]::GetEnvironmentVariable("XbimEssentials", "User")
-if($version -gt $installed){
-	[Environment]::SetEnvironmentVariable("XbimEssentials", $version, "User")
-	Start-Process "https://goo.gl/forms/S2u2YvPBrwkIUgXm1"
-}
+# $version = $package.Version
+# $installed = [Environment]::GetEnvironmentVariable("XbimEssentials", "User")
+# if($version -gt $installed){
+# 	[Environment]::SetEnvironmentVariable("XbimEssentials", $version, "User")
+# 	Start-Process "https://goo.gl/forms/S2u2YvPBrwkIUgXm1"
+# }
