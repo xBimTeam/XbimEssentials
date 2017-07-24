4af12147b0335f35d22268a006eb7f6d38f4c710
Stephen Lockley <steve.lockley@xbim.net>
2017-07-24 07:19:55 +0100

    Ported to Net Standard 1.6 and Framework 4.6.2

---
+++ Xbim.IO.MemoryModel.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Xbim.IO.MemoryModel.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
