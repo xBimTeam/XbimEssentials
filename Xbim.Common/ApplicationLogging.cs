3313b8422ee45948dd10c3a53ec1eba77e43bfd7
Stephen Lockley <steve.lockley@xbim.net>
2017-08-04 08:52:47 +0100

    Logger Factory cached

--- Xbim.Common/ApplicationLogging.cs
+++ Xbim.Common/ApplicationLogging.cs
@@ -4,7 +4,23 @@ namespace Xbim.Common
 {
     public static class ApplicationLogging
     {
-        public static ILoggerFactory LoggerFactory { get; } = new LoggerFactory();
+        private static ILoggerFactory _factory = null;
+
+        
+        public static ILoggerFactory LoggerFactory
+        {
+            get
+            {
+                if (_factory == null)
+                {
+                    _factory = new LoggerFactory();
+                    
+                }
+                return _factory;
+            }
+            set { _factory = value; }
+        }
+        
         public static ILogger CreateLogger<T>() =>  LoggerFactory.CreateLogger<T>();
     }
 }
