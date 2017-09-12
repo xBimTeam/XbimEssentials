8aca336a52287f1211dcd237cb0dbc9651e3aa84
Stephen Lockley <steve.lockley@xbim.net>
2017-09-12 16:40:22 +0100

    Attempt to resolve net47 zip file bindings

--- Xbim.IO.MemoryModel/Resources.Designer.cs
+++ Xbim.IO.MemoryModel/Resources.Designer.cs
@@ -10,7 +10,6 @@
 
 namespace Xbim.IO.MemoryModel {
     using System;
-    using System.Reflection;
     
     
     /// <summary>
@@ -40,7 +39,7 @@ namespace Xbim.IO.MemoryModel {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Xbim.IO.MemoryModel.Resources", typeof(Resources).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Xbim.IO.MemoryModel.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
