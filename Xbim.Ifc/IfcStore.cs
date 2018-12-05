14cdff31946fc0395ad127fee27a92a48e8c9496
Aurélien <aurelien.morel@ubiant.com>
2018-12-05 10:01:04 +0100

    [FIXED] Fix missing FileName patching during SaveAs method

--- Xbim.Ifc/IfcStore.cs
+++ Xbim.Ifc/IfcStore.cs
@@ -824,7 +824,8 @@ namespace Xbim.Ifc
         /// <param name="actualFormat">this will be correctly set</param>
         /// <param name="progDelegate"></param>
         private void SaveAs(string actualFileName, StorageType actualFormat, ReportProgressDelegate progDelegate)
-        {
+          {
+            FileName = actualFileName;
             if (actualFormat.HasFlag(StorageType.Xbim)) //special case for xbim
             {
                 var ef = GetFactory(SchemaVersion);
