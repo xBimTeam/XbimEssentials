2d10912890e6fb4e42e87b5291d77077a12fa1dc
Martin Cerny <martin.cerny@xbim.cz>
2020-11-02 10:05:24 +0100

    MemoryModel aware of IFC4x3

--- Xbim.Common/Model/StepModel.cs
+++ Xbim.Common/Model/StepModel.cs
@@ -614,11 +614,6 @@ namespace Xbim.Common.Model
         /// </summary>
         protected virtual void Dispose(bool native)
         {
-
-        }
-
-        public void Dispose()
-        {
             _instances.Dispose();
             _transactionReference = null;
             _cacheReference = null;
@@ -631,7 +626,10 @@ namespace Xbim.Common.Model
             _newEntityHandlers.Clear();
             _modifiedEntityHandlers.Clear();
             _deletedEntityHandlers.Clear();
+        }
 
+        public void Dispose()
+        {
             Dispose(true);
             GC.SuppressFinalize(this);
             return;
