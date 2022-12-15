af7c7af479f9a7ad102463c3ef2ac27627b751da
Martin Cerny <martin.cerny@xbim.cz>
2022-12-15 08:41:16 +0100

    test: verify that IfcStore can open IFC4x3 files

--- Tests/Ifc4x3ReadingTests.cs
+++ Tests/Ifc4x3ReadingTests.cs
@@ -10,6 +10,7 @@ using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 using Xbim.Common;
+using Xbim.Ifc;
 using Xbim.Ifc4x3;
 using Xbim.IO.Memory;
 using Xunit;
@@ -32,7 +33,7 @@ namespace Xbim.Essentials.Tests
         [InlineData(@"TestFiles\IFC4x3\test0.ifc")]
         [InlineData(@"TestFiles\IFC4x3\test1.ifc")]
         [InlineData(@"TestFiles\IFC4x3\test2.ifc")]
-        public void CanReadSampleFiles(string file)
+        public void CanParseSampleFiles(string file)
         {
             var config = new LoggerConfiguration()
                 .MinimumLevel.Information()
@@ -50,5 +51,36 @@ namespace Xbim.Essentials.Tests
             var errors = model.LoadStep21(stream, stream.Length);
             errors.Should().Be(0, "There should be no errors.");
         }
+
+        [Theory]
+        [InlineData(@"TestFiles\IFC4x3\DirectrixDerivedReferenceSweptAreaSolid-1.ifc")]
+        [InlineData(@"TestFiles\IFC4x3\DirectrixDerivedReferenceSweptAreaSolid-2.ifc")]
+        [InlineData(@"TestFiles\IFC4x3\FixedReferenceSweptAreaSolid-1.ifc")]
+        [InlineData(@"TestFiles\IFC4x3\Header example.ifc")]
+        [InlineData(@"TestFiles\IFC4x3\SectionedSolidHorizontal-1.ifc")]
+        [InlineData(@"TestFiles\IFC4x3\SectionedSolidHorizontal-2.ifc")]
+        [InlineData(@"TestFiles\IFC4x3\SectionedSolidHorizontal-3.ifc")]
+        [InlineData(@"TestFiles\IFC4x3\SectionedSolidHorizontal-4.ifc")]
+        [InlineData(@"TestFiles\IFC4x3\SectionedSolidHorizontal-5.ifc")]
+        [InlineData(@"TestFiles\IFC4x3\SectionedSolidHorizontal-6.ifc")]
+        [InlineData(@"TestFiles\IFC4x3\test0.ifc")]
+        [InlineData(@"TestFiles\IFC4x3\test1.ifc")]
+        [InlineData(@"TestFiles\IFC4x3\test2.ifc")]
+        public void IfcStoreCanOpenSampleFiles(string file)
+        {
+            var config = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console()
+                .WriteTo.Debug()
+                .CreateLogger();
+            var logger = (new LoggerFactory()).AddSerilog(config).CreateLogger(typeof(IModel));
+
+            var exception = Record.Exception(() =>
+            {
+                using var model = IfcStore.Open(file);
+            });
+            exception.Should().BeNull();
+        }
+
     }
 }
