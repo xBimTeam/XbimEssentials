8b0297fbff131a30399ed73e9dbd3bb2c530ba76
santiagoIT <sburbano@itworks.ec>
2024-06-11 03:28:08 -0500

    Issue 566 ifc composite curve.segments  (#570)
    * Implement IIfcCompositeCurve.Segments for Ifc4x3.GeometryResource.IfcCompositeCurve
    
    * Updated IIfcCompositeCurve.Segments to avoid copying contents each access
    
    ---------
    
    Co-authored-by: Santiago Burbano <santiago.burbano@hsbcad,com>
    Co-authored-by: Andy Ward <andy.ward@xbim.net>

--- Xbim.Ifc4x3/Interfaces/IFC4/IfcCompositeCurve.cs
+++ Xbim.Ifc4x3/Interfaces/IFC4/IfcCompositeCurve.cs
@@ -23,17 +23,24 @@ namespace Xbim.Ifc4x3.GeometryResource
 
 		[CrossSchemaAttribute(typeof(IIfcCompositeCurve), 1)]
 		IItemSet<IIfcCompositeCurveSegment> IIfcCompositeCurve.Segments 
-		{ 
-			get
-			{
-				//## Handle return of Segments for which no match was found
-				//TODO: Handle return of Segments for which no match was found
-				throw new System.NotImplementedException();
-				//##
-			} 
-		}
+		{
+            get
+            {
+                //## Custom code - not auto generated
 
-		[CrossSchemaAttribute(typeof(IIfcCompositeCurve), 2)]
+                return _segmentsExtended ?? (_segmentsExtended = new Common.Collections.ExtendedItemSet<IfcSegment, IIfcCompositeCurveSegment>(
+                    Segments,
+                    new ItemSet<IIfcCompositeCurveSegment>(this, 0, -16),
+                    ifc4x3 => ifc4x3 as IIfcCompositeCurveSegment,
+                    ifc4if => ifc4if as IfcCompositeCurveSegment));
+                //##
+            }
+        }
+
+        //private field to hold any extended data
+        private IItemSet<IIfcCompositeCurveSegment> _segmentsExtended;
+
+        [CrossSchemaAttribute(typeof(IIfcCompositeCurve), 2)]
 		Ifc4.MeasureResource.IfcLogical IIfcCompositeCurve.SelfIntersect 
 		{ 
 			get
