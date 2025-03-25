711e1390e362590536c264a25f34b2b463a9c0f7
Andy Ward <andy.ward@xbim.net>
2025-03-25 17:13:45 +0000

    Fix parsing of invalid escape sequences in string literals (#606)
    * Updated Step21 Parser (Scanner/Lexer) to more gracefully handle invalid string literal inputs as per #605  #586 etc
    
    Refactored the lex code to make more maintainable.
    Refactored the scanner code gen approach to enable us to retain local fixes to the Scanner
    Added gplex materials for reference. Documented gplex usage
    
    Improved support for multi-line string literals with invalid escaping
    
    i.e. IFCLABEL('Text
    over multiple line without slash X encoding')
    
    These may be technically valid but handling them as an edge case creates parsing issues for other more typical classes of errors and edge cases
    (e.g. unescaped quotes, unescaped backslashes, incorrect \X \S and \P char encoding etc) -
    because the parser can fail to backtrack until hitting EOF as it attempts to find a valid multi-line terminated string. Essentially one valid unterminated string renders the whole file a failure.
    By eliminating unencoded CRLF in string literals we fail early affecting only the single entity.
    
    
    * Re-added support for parsing multi-line literal strings
    Added warning for invalid escaping in strings
    
    * Added test to ensure string encoding is correct on output
    
    * Added tests for invalid FILE_NAME escaping in header

---
+++ Tests/XbimP21StringDecoderTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Xbim.IO.Step21;
+using Xunit;
+
+namespace Xbim.Essentials.Tests
+{
+    public class XbimP21StringDecoderTests
+    {
+        [InlineData("", "")]
+        [InlineData("''", "'")]
+        [InlineData("'Acme'", "'Acme'")]
+        [InlineData("O''Leary", "O'Leary")]
+        [InlineData("O'Leary", "O'Leary")]
+        [InlineData(@"http://foo.bar.com/Abc?c=d", "http://foo.bar.com/Abc?c=d")]
+        [InlineData(@"\S\Drger", @"Ärger")]     // D = 0x44. 0x44 + 0x80(top bit) = 0xC4 => Ä (in ISO-8859-1)
+        [InlineData(@"h\S\ttel", @"hôtel")]     // t = 0x74. + 0x80 => 0xF4 = ô (in ISO-8859-1)
+        [InlineData(@"\PA\h\S\ttel", @"hôtel")]     // t = 0x74. + 0x80 => 0xF4 = ô (in ISO-8859-1) - Explicitly state 8859-1 codepage
+        [InlineData(@"\PE\h\S\ttel", @"hєtel")] // t = 0x74. + 0x80 => 0xF4 = є (in ISO-8859-5) P*E* => 5th 8859 codepage (Cyrillic)
+        [InlineData(@"\X2\30B330F330AF30EA30FC30C84F537A4D\X0\", "コンクリート体積")]
+        [InlineData(@"file://ATTRIBUTE\X2\30B330F330AF30EA30FC30C84F537A4D8868\X0\.XLSX", @"file://ATTRIBUTEコンクリート体積表.XLSX")]
+        [InlineData(@"file://ATTRIBUTE/\X2\30B330F330AF30EA30FC30C84F537A4D8868\X0\.XLSX", @"file://ATTRIBUTE/コンクリート体積表.XLSX")]
+        [InlineData(@"file://ATTRIBUTE\\\X2\30B330F330AF30EA30FC30C84F537A4D8868\X0\.XLSX", @"file://ATTRIBUTE\コンクリート体積表.XLSX")]
+        [InlineData(@"file://ATTRIBUTE\\X2\30B330F330AF30EA30FC30C84F537A4D8868\X0\.XLSX", 
+                         @"file://ATTRIBUTE\X2\30B330F330AF30EA30FC30C84F537A4D8868\X0\.XLSX")] // Invalid encoding. Only \\ un-escaped
+        [InlineData(@"F:\\Dropbox\\00 - ndBIM_Coordena\S\C\S\'\S\C\S\#o\\01 - Parcerias Comerciais\\YellowBox\\2015-09-24\\IFC\\ARQ.ifc", 
+                         @"F:\Dropbox\00 - ndBIM_CoordenaÃ§Ã£o\01 - Parcerias Comerciais\YellowBox\2015-09-24\IFC\ARQ.ifc")]
+        [InlineData(@"Em\X\F4ji\X4\0001F44D0001F604\X0\\X2\2B06FE0F\X0\", @"Emôji👍😄⬆️")]
+        
+        [Theory]
+        public void CanDecodeString(string p21input, string expectedOutput)
+        {
+            var decoder = new XbimP21StringDecoder();
+
+            var result = decoder.Unescape(p21input);
+
+            result.Should().Be(expectedOutput);
+
+        }
+    }
+}
+
+
